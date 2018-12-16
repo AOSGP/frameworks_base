f61e804237b2e42c003e7c1c77f018f86b002906
dimusa6 <dimusa.dms@gmail.com>
2018-12-16 15:21:31 +0100

    Revert "base: fix Advanced reboot menu bg"
    This reverts commit cd577401dbbfa212251278baf8c8caf34a0c522b.

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -779,7 +779,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
         public View getView(int position, View convertView, ViewGroup parent) {
             Action action = getItem(position);
             View view = action.create(mContext, convertView, parent, LayoutInflater.from(mContext));
-            if (position == 3) {
+            if (position == 2) {
                 HardwareUiLayout.get(parent).setDivisionView(view);
             }
             return view;
