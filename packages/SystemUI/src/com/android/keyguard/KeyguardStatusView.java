33453e9648704355eb7c277e57dc6226cc507ba9
dimusa6 <dimusa.dms@gmail.com>
2018-12-16 19:17:52 +0100

    Revert "Always keep clock/date & alarm visible while dozing"
    This reverts commit bd3bf51421147c3138fb4d8a978c535e3cfefcc4.

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -26,13 +26,7 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.PowerManager;
-import android.os.RemoteException;
-import android.os.ServiceManager;
 import android.os.UserHandle;
-import android.provider.Settings;
-import android.service.dreams.DreamService;
-import android.service.dreams.IDreamManager;
 import android.support.v4.graphics.ColorUtils;
 import android.text.TextUtils;
 import android.text.format.DateFormat;
@@ -290,40 +284,6 @@ public class KeyguardStatusView extends GridLayout {
         return false;
     }
 
-    private void updateSettings(boolean forceHide) {
-        final ContentResolver resolver = getContext().getContentResolver();
-        final Resources res = getContext().getResources();
-        AlarmManager.AlarmClockInfo nextAlarm =
-                mAlarmManager.getNextAlarmClock(UserHandle.USER_CURRENT);
-        boolean showAlarm = Settings.System.getIntForUser(resolver,
-                Settings.System.HIDE_LOCKSCREEN_ALARM, 1, UserHandle.USER_CURRENT) == 1;
-        boolean showClock = Settings.System.getIntForUser(resolver,
-                Settings.System.HIDE_LOCKSCREEN_CLOCK, 1, UserHandle.USER_CURRENT) == 1;
-        boolean showDate = Settings.System.getIntForUser(resolver,
-                Settings.System.HIDE_LOCKSCREEN_DATE, 1, UserHandle.USER_CURRENT) == 1;
-
-        mClockView = (TextClock) findViewById(R.id.clock_view);
-        mDateView = (DateView) findViewById(R.id.date_view);
-        mAlarmStatusView = (TextView) findViewById(R.id.alarm_status);
-
-        if (!isDozeMode()) {
-            mClockView.setVisibility(showClock ? View.VISIBLE : View.GONE);
-        } else {
-            mClockView.setVisibility(View.VISIBLE);
-        }
-
-        if (!isDozeMode()) {
-            mDateView.setVisibility(showDate ? View.VISIBLE : View.GONE);
-        } else {
-            mDateView.setVisibility(View.VISIBLE);
-        }
-
-        if (!isDozeMode()) {
-            mAlarmStatusView.setVisibility(showAlarm && nextAlarm != null ? View.VISIBLE : View.GONE);
-        }
-        updateDozeVisibleViews();
-    }
-
     // DateFormat.getBestDateTimePattern is extremely expensive, and refresh is called often.
     // This is an optimization to ensure we only recompute the patterns when the inputs change.
     private static final class Patterns {
@@ -386,37 +346,16 @@ public class KeyguardStatusView extends GridLayout {
         int blendedAlarmColor = ColorUtils.blendARGB(mAlarmTextColor, Color.WHITE, darkAmount);
         mAlarmStatusView.setTextColor(blendedAlarmColor);
         mAlarmStatusView.setCompoundDrawableTintList(ColorStateList.valueOf(blendedAlarmColor));
-        refresh();
     }
 
     public void setPulsing(boolean pulsing) {
         mPulsing = pulsing;
         updateDozeVisibleViews();
-        refresh();
     }
 
     private void updateDozeVisibleViews() {
         for (View child : mVisibleInDoze) {
             child.setAlpha(mDarkAmount == 1 && mPulsing ? 0.8f : 1);
         }
-    }
-
-    private boolean isDozeMode() {
-        IDreamManager dreamManager = getDreamManager();
-        PowerManager pm = (PowerManager) getContext().getSystemService(Context.POWER_SERVICE);
-        try {
-            //noinspection deprecation
-            if (dreamManager != null && dreamManager.isDozing() && pm != null && !pm.isScreenOn()) {
-                return true;
-            }
-        } catch (RemoteException e) {
-            return false;
-        }
-        return false;
-    }
-
-    static IDreamManager getDreamManager() {
-        return IDreamManager.Stub.asInterface(
-                ServiceManager.checkService(DreamService.DREAM_SERVICE));
     }
 }
